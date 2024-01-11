7f80fdec615455b2003c396b6716d53a1b9e2739
Andreza <andreza@multiplier.com.br>
2024-01-11 12:17:16 -0300

    feat: realizar tudo dentro do callback

--- javascripts/Callback/ex2.js
+++ javascripts/Callback/ex2.js
@@ -1,15 +1,19 @@
-function newButton(text) {
+function newButton(text, callback) {
     const $body = document.querySelector("body");
     const $button = document.createElement("button");
+   
     $button.textContent = text;
+
+    callback();
     $body.insertAdjacentElement('beforeend', $button);
 
     return $button;
 }
 
-const login = newButton('Login');
-const signup = newButton('Signup');
-
-login.style.cssText = `
-    font-size: 10px;
-`;
+//estou passando para dentro da função newButton outra função por parametro
+newButton("Login", () => {
+    console.log("funcionou!");
+});
+newButton("Signup", () => {
+    console.log("funcionou 2");
+});
