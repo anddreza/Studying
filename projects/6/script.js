60ce0fe1b5721ff6424b91401e81e859d80b9584
Andreza <andreza@multiplier.com.br>
2024-01-22 08:16:19 -0300

    feat: adição informações do projeto

---
+++ projects/6/script.js
@@ -0,0 +1,26 @@
+
+var video = document.querySelector('video');
+
+navigator.mediaDevices.getUserMedia({video: true})
+    .then(stream => {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch( error => {
+        console.log(error);
+    })
+
+document.querySelector('button').addEventListener('click', () => {
+   var canvas = document.querySelector('canvas');
+   canvas.height = video.videoHeight;
+   canvas.width = video.videoWidth;
+   //inserir de fato a foto na foto canvas
+   var context = canvas.getContext('2d');
+   context.drawImage(video, 0, 0); 
+   //download
+   var link = document.createElement('a');
+   link.download = 'foto.png';
+   link.href = canvas.toDataURL();
+   link.textContent = 'Clique para baixar a imagem';
+   document.body.appendChild(link);
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
 Projetos Javascript #01 - Criando um Relógio Digital com Javascript
 https://www.youtube.com/watch?v=mOocNybhio4&list=PLJ8PYFcmwFOxmqYNlo_H8TYVSDLxB8HdR&ab_channel=Jo%C3%A3oTinti
+
+Projeto #4 e #5 - Como obter a localização e criar um mapa com JS:
+https://youtu.be/ZFge8WrnWHo?si=xnyxHoJeVjnbH9RS
+
+Projeto Javascript #06 - Como utilizar a câmera e tirar uma foto com JS:
+https://youtu.be/CBWkMNo6px8?si=7VOg6lyMdqEh5dDu
+
+Projeto Javascript #07 - Como gerar um QRCode com JS:
+https://youtu.be/E1gPN6LmH00?si=nycjwwQqVuikYz9c
