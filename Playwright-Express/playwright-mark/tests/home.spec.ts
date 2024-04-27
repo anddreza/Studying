70fc53329366dd495bbdcf08b11802ba8d514170
Andreza <andreza@multiplier.com.br>
2024-04-26 21:13:43 -0300

    feat: exemplo

---
+++ Playwright-Express/playwright-mark/tests/home.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+
+test('webapp deve estar online')
+
+
+
+function mostrarNome(){
+    console.log('Fernando Papito')
+}
+
+const imprimeNome = () => {
+    console.log('Fernando Papito')
+}
