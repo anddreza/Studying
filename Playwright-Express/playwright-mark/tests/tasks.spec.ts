2a9ece3d2cd9c9f122801398560adb0fc88734b1
Andreza <andreza@multiplier.com.br>
2024-04-26 23:28:51 -0300

    feat: test with faker

--- Playwright-Express/playwright-mark/tests/tasks.spec.ts
+++ Playwright-Express/playwright-mark/tests/tasks.spec.ts
@@ -1,4 +1,6 @@
 import {test, expect} from '@playwright/test'
+import { faker } from '@faker-js/faker'
+
 
 test('deve poder cadastrar uma nova tarefa', async({page}) => {
     
@@ -7,7 +9,13 @@ test('deve poder cadastrar uma nova tarefa', async({page}) => {
    //await page.fill('input[class*=InputNewTask]') : outra forma de encontrar 
   // await page.fill('input[placeholder="Add a new Task"]', 'Ler um livro de TS') //This is substitute
     const inputTaskName = page.locator('input[class*=InputNewTask]')
-    await inputTaskName.fill('Ler um livro de TS')
-    await page.fill('input[class*=InputNewTask]', 'Ler um livro de TS')
+    //await inputTaskName.fill('Ler um livro de TS')
+   // await page.fill('input[class*=InputNewTask]', 'Ler um livro de TS')
+   await inputTaskName.fill(faker.lorem.words())
 
+//    await inputTaskName.press('Enter') //press the button enter
+//    await page.click('xpath=//button[contains(text(), "Create")]'): XPath, it's not a best option
+    
+    await page.click('css=button >> text=Create')
+    
 })
