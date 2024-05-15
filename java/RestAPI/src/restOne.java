7a9bba2669f490707fd7dc995ba3e0b56d1c5969
Andreza <andreza@multiplier.com.br>
2024-05-14 21:12:41 -0300

    feat: learn about selenium

--- java/RestAPI/src/restOne.java
+++
@@ -1,7 +0,0 @@
-import io.restassured.RestAssured.*;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
-
-public class restOne {
-
-}
---
+++ java/selenium/src/main/java/pages/HomePage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+    //private By formAuthenticationLink = By.linkText("Form Authentication");
+
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+    public LoginPage clickFormAuthentication(){
+       clickLink("Form Authentication");
+        // driver.findElement(formAuthenticationLink).click();
+        return new LoginPage(driver);
+
+    }
+
+    public DropdownPage clickDropDown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
