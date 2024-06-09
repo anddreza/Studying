91089791811c41816526a0fb6fb44994eb3ee5c5
Andreza <andreza@multiplier.com.br>
2024-06-09 08:28:26 -0300

    feat: estudos

---
+++ java/junit5/src/test/java/junit5tests/ParameterizedTests.java
@@ -0,0 +1,49 @@
+package junit5tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+public class ParameterizedTests {
+
+    @ParameterizedTest(name = "Run: {index} - value: {arguments}")
+    @ValueSource(ints = { 1,5,6 })
+    void intValues(int theParam){
+        System.out.println("theParam = " + theParam);
+    }
+
+    @ParameterizedTest
+   // @NullSource
+   // @EmptySource
+    @NullAndEmptySource
+    @ValueSource(strings = { "firstString", "secondString"})
+    void stringValues(String theParam){
+        System.out.println("theParam = " + theParam);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"Steve, Rogers", "captain, marvel", "bucky, barnes"})
+    void csvSource_StringString(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve, 32, true", "captain, 21, false", "bucky, 5, true"})
+    void csvSource_StringIntBoolean(String param1, int param2, Boolean param3){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"captain america, 'steve, rogers'", "winter soldier" +
+            "'bucky, barnes'"})
+    void csvSource_StringWithComa(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"steve?rogers", "bucky?barnes"}, delimiter = '?')
+    void csvSource_StringWithDiffDelimiter(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+}
--- robot-express/mark85-robot-express/resources/libs/database.py
+++ robot-express/mark85-robot-express/resources/libs/database.py
@@ -1,9 +1,21 @@
 from robot.api.deco import keyword
 from pymongo import MongoClient 
+import bcrypt
 
 client = MongoClient('mongodb+srv://qa:xperience@cluster0.xykpcik.mongodb.net/markdb?retryWrites=true&w=majority&appName=Cluster0')
 
 db = client['markdb']
+
+@keyword('Clean user from database')
+def clean_user(user_email):
+    users = db['users']
+    tasks = db['tasks']
+
+    u = users.find_one({'email': user_email})
+
+    if (u):
+        tasks.delete_many({'user': u['_id']})
+        users.delete_many({'email': user_email})
 
 @keyword('Remove user from database')
 def remove_user(email):
@@ -13,12 +25,15 @@ def remove_user(email):
 
 @keyword('Insert user from database')
 def insert_user(user):
-    # doc = {
-    #     'name': name,
-    #     'email': email,
-    #     'password': password
-    # }
+
+    hash_pass = bcrypt.hashpw(user['password'].encode('utf-8'), bcrypt.gensalt(8))
+
+    doc = {
+        'name': user['name'],
+        'email': user['email'],
+        'password': hash_pass
+    }
 
     users = db['users']
-    users.insert_one(user)
+    users.insert_one(doc)
     print(user)
